8277f2a9423385690416e7fd579669f2c6fd447b
unknown <telus-sgupta@premium-cloud-support.com>
2015-02-05 14:33:33 -0500

    Testing

---
+++ FirstFile.go
@@ -0,0 +1,17 @@
+package FirstFile
+
+func Sub(x, y int) int {
+	return x - y
+}
+
+func Mul(x, y int) int {
+	return x * y
+}
+
+func Add(x, y int) int {
+	return x + y
+}
+
+func Div(x, y int) int {
+	return x / y
+}
